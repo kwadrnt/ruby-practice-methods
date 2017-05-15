e942ea0749dc63d32455381657a404b278cf866f
kwadrnt <kevinctse@gmail.com>
2017-05-15 16:04:41 -0700

    initial method notes

---
+++ ruby_methods.rb
@@ -0,0 +1,12 @@
+# 1) Write a method called p_times that takes a statement and a num puts the statement some num of times to the console.
+def p_times (statement, num)
+  puts num.times * statement
+end
+
+p_times "word" 5
+
+# 2) Write a method called letter_count to count letter occurrence in a string, returned as a Hash. For example, letter_count 'apple' would return {"a"=>1, "p"=>2, "l"=>1, "e"=>1}
+
+# 3) Write a method called mock_me that gets some input from the terminal and puts it until the users type quit or q in the terminal. (Be sure to remove trailing \n.)
+
+# 4) Write a method called print_contacts that takes a hash of name and phone-number key-value pairs and puts the name and phone-number for each contact.
