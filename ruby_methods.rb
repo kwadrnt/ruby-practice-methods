dee66033c623977f9f1f0c60a482e98c2077db5f
kwadrnt <kevinctse@gmail.com>
2017-05-15 16:53:54 -0700

    working on loop#3

--- ruby_methods.rb
+++ ruby_methods.rb
@@ -10,19 +10,27 @@ p_times('high five',3)
 # 2) Write a method called letter_count to count letter occurrence in a string, returned as a Hash. For example, letter_count 'apple' would return {"a"=>1, "p"=>2, "l"=>1, "e"=>1}
 def count_letters(string)
   count = Hash.new(0)
-  string.delete(" ").each_char { |char|  count[char]+=1}
+  string.delete(" ").each_char { |letter|  count[letter]+=1}
   Hash[count]
 end
 
-print count_letters("ruby is a super rad language")
+print count_letters("nba playoffs")
 
 # 3) Write a method called mock_me that gets some input from the terminal and puts it until the users type quit or q in the terminal. (Be sure to remove trailing \n.)
 def mock_me input
-  # puts input UNTIL users type Quit or q
-  # until iterator
+input = nil
+  while input != "quit" && input != "q"
+    puts input
+    puts "Enter input here:"
+    input = gets.chomp
+  end
 end
 
-# 4) Write a method called print_contacts that takes a hash of name and phone-number key-value pairs and puts the name and phone-number for each contact.
-def print_contacts
+mock_me google
+# Write a method for each exercise below that uses Array#map:
+# 1.) Write a method called multiply_by that takes a number and returns an array of numbers multiplied by that number.
 
-end
+# Method Exercises
+# 1.) Write a method to compute the factorial of a number.
+
+# 2.) Write a method to check if a number is a palindrome.
